c4f76b2f3d833ce16ae5657348aef64af25e5841
Sanjaypal1916 <sanjaypal606060@gmail.com>
2024-08-12 13:49:30 +0530

    this is the first project push on github

---
+++ src/main/java/com/project/videostreamer/VideostreamerApplication.java
@@ -0,0 +1,13 @@
+package com.project.videostreamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideostreamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideostreamerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/videostreamer/VideostreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.videostreamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideostreamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
